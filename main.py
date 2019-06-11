2460a296d92424119513473f170494a85ba0e407
Sebastian Andersen <sebastian@sandersen.co>
2019-06-11 11:37:01 +0200

    Just like this.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 parser = argparse.ArgumentParser(prog="what")
 
 
-def start_timer():
-    print("fisk")
+def start_timer(a):
+    print(a)
 
 
 def stop_timer():
@@ -23,6 +23,14 @@ parser.add_argument(
     action="store_true",
 )
 
+parser.add_argument(
+    "string",
+    metavar="S",
+    type=str,
+    nargs="?",
+    help="This is very important, use it right my man",
+)
+
 args = parser.parse_args()
 
 # print(args)
@@ -32,4 +40,4 @@ if len(sys.argv) == 1:
     parser.print_help()
 
 if args.start:
-    print("whoo")
+    start_timer(args.string)
